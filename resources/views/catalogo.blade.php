21c99ada718e58b94dcad6fb98bf37af485bb245
andresfelipecespedesgomez@gmail.com <andresfelipecespedesgomez@gmail.com>
2024-08-17 21:04:34 -0500

    mostrar productos de la bd en catalogo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -3,53 +3,62 @@
 @section('content')
 <div class="containerCatalogo">
 
-    <h3 class="title"> vegetales y verduras frescas </h3>
+    <h3 class="title"> Vegetales y Verduras Frescas </h3>
 
     <div class="products-container">
-        <a href="InfoProduc.html">
-            <div class="product" data-name="p-1">
-                <img src="{{asset('img/1.png')}}" alt="">
-                <h3>Fresas</h3>
-                <div class="price">$5.000</div>
+        
+        <!-- Primer Producto (posición 1 del array) -->
+        @if(isset($productos[0]))
+            <a href="InfoProduc.html">
+                <div class="product" data-name="p-1">
+                    <img src="{{ asset('img/1.jpg') }}" alt="">
+                    <h3>{{ $productos[0]->nombre }}</h3>
+                    <div class="price">${{ number_format($productos[0]->precio, 0, ',', '.') }}</div>
+                </div>
+            </a>
+        @endif
+
+        <!-- Segundo Producto (posición 2 del array) -->
+        @if(isset($productos[1]))
+            <div class="product" data-name="p-2">
+                <img src="{{ asset('img/2.jpg') }}" alt="">
+                <h3>{{ $productos[1]->nombre }}</h3>
+                <div class="price">${{ number_format($productos[1]->precio, 0, ',', '.') }}</div>
             </div>
-        </a>
+        @endif
 
-        <div class="product" data-name="p-2">
-            <img src="{{asset('img/2.png')}}" alt="">
-            <h3>Cebollas</h3>
-            <div class="price">$4.000</div>
-        </div>
+        <!-- Tercer Producto (posición 3 del array) -->
+        @if(isset($productos[2]))
+            <div class="product" data-name="p-3">
+                <img src="{{ asset('img/3.jpg') }}" alt="">
+                <h3>{{ $productos[2]->nombre }}</h3>
+                <div class="price">${{ number_format($productos[2]->precio, 0, ',', '.') }}</div>
+            </div>
+        @endif
 
-        <div class="product" data-name="p-3">
-            <img src="{{asset('img/3.png')}}" alt="">
-            <h3>Tomates</h3>
-            <div class="price">$2.00</div>
-        </div>
+        <!-- Cuarto Producto (posición 4 del array) -->
+        @if(isset($productos[3]))
+            <div class="product" data-name="p-4">
+                <img src="{{ asset('img/4.jpg') }}" alt="">
+                <h3>{{ $productos[3]->nombre }}</h3>
+                <div class="price">${{ number_format($productos[3]->precio, 0, ',', '.') }}</div>
+            </div>
+        @endif
 
-        <div class="product" data-name="p-4">
-            <img src="{{asset('img/4.png')}}" alt="">
-            <h3>Berenjenas</h3>
-            <div class="price">$2.00</div>
-        </div>
-
-        <div class="product" data-name="p-5">
-            <img src="{{asset('img/5.png')}}" alt="">
-            <h3>Brócoli</h3>
-            <div class="price">$6.000</div>
-        </div>
-
-        <div class="product" data-name="p-6">
-            <img src="{{asset('img/6.png')}}" alt="">
-            <h3>Papa capira </h3>
-            <div class="price">$2.000</div>
-        </div>
+        <!-- Quinto Producto (posición 5 del array) -->
+        @if(isset($productos[4]))
+            <div class="product" data-name="p-5">
+                <img src="{{ asset('img/5.jpg') }}" alt="">
+                <h3>{{ $productos[4]->nombre }}</h3>
+                <div class="price">${{ number_format($productos[4]->precio, 0, ',', '.') }}</div>
+            </div>
+        @endif
 
     </div>
 
 </div>
 @endsection
 
-
 @section('style')
-<link rel="stylesheet" href="{{asset('css/catalogo.css')}}">
+<link rel="stylesheet" href="{{ asset('css/catalogo.css') }}">
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ Route::view('/recuperar','olvidoClv')->name('recuperar');
 Route::view('/info','infoProduc')->name('info');
 Route::view('/tarjeta','tarjeta')->name('tarjeta');
 Route::view('/carrito','carrito')->name('carrito');
-Route::view('/cata','catalogo')->name('catalogo');
+Route::get('/catalogo', [ProductoController::class, 'index']);
 Route::view('/perfil','verPerfil')->name('verPerfil');
 Route::view('/datos','Datos')->name('misDatos');
-Route::get('/catalogo', [ProductoController::class, 'index']);
 
 
 
